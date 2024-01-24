751baf248983e11eb0c67b8d343b7a9c61542084
Hristina <126361505+hristina71@users.noreply.github.com>
2024-01-24 22:23:42 +0100

    dodavanje template method paterna za unos paketa u bazu

---
+++ WinFormsApp/WinFormsApp/InternetPaketDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp
+{
+    internal class InternetPaketDb : PaketDb
+    {
+        public override string setSelectAllQuery()
+        {
+            String query = @"select *
+                           from InternetPaket";
+
+            return query;
+        }
+
+        public override List<Paket> executeSelectAllQuery(String query)
+        {
+            List<Paket> paketi = new List<Paket>();
+
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataReader reader = new SqlCommand().ExecuteReader();
+
+            while (reader.Read())
+            {
+                paketi.Add(new InternetPaket());
+                /*    paketi.Add(new InternetPaket(
+                                                   int.Parse(reader["id"].ToString()),
+                                                   int.Parse(reader["naziv"].ToString()),
+                                                   int.Parse(reader["cena"].ToString()),
+                                                   int.Parse(reader["brzina_interneta"].ToString())
+                       ));*/
+            }
+
+            return paketi;
+        }
+    }
+}
---
+++ WinFormsApp/WinFormsApp/KombinovaniPaketDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp
+{
+    internal class KombinovaniPaketDb : PaketDb
+    {
+        public override string setSelectAllQuery()
+        {
+            String query = @"SELECT *
+                           FROM KombinovaniPaketi";
+
+            return query;
+        }
+
+        public override List<Paket> executeSelectAllQuery(String query)
+        {
+            List<Paket> paketi = new List<Paket>();
+
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataReader reader = new SqlCommand().ExecuteReader();
+
+            while(reader.Read())
+            {
+                paketi.Add(new KombinovaniPaket());
+            /*    paketi.Add(new KombinovaniPaket(
+                                               int.Parse(reader["id"].ToString()),
+                                               int.Parse(reader["naziv"].ToString()),
+                                               int.Parse(reader["cena"].ToString()),
+                                               int.Parse(reader["tv_paket_id"].ToString()),
+                                               int.Parse(reader["internet_paket_id"].ToString())
+                   ));*/ 
+            }
+
+            return paketi;
+        }
+
+    }
+}
