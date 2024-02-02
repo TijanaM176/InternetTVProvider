a95fd7e0ebe6613d61b2c5ec289c723bcd574df7
tmarkovic176 <117023727+TijanaM176@users.noreply.github.com>
2024-02-02 17:18:32 +0100

    Odradjen Klijent u Builder patternu

---
+++ InternetTVSolution/InternetTVProviderLibrary/BuilderPattern/ClientBuilder.cs
@@ -0,0 +1,47 @@
+﻿using InternetTVProviderLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTVProviderLibrary
+{
+    public class ClientBuilder:IClientBuilder
+    {
+        private string Username;
+        private string FirstName;
+        private string LastName;
+        private int Id;
+
+
+        public IClientBuilder SetUsername(string username)
+        {
+            this.Username = username;
+            return this;
+        }
+
+        public IClientBuilder SetIme(string ime)
+        {
+            this.FirstName = ime;
+            return this;
+        }
+
+        public IClientBuilder SetPrezime(string prezime)
+        {
+            this.LastName = prezime;
+            return this;
+        }
+
+        public IClientBuilder SetId(int id)
+        {
+            this.Id = id;
+            return this;
+        }
+
+        public Client Build()
+        {
+            return new Client(Username, FirstName, LastName, Id);
+        }
+    }
+}
---
+++ InternetTVSolution/InternetTVProviderLibrary/BuilderPattern/IClientBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTVProviderLibrary
+{
+    public interface IClientBuilder
+    {
+        IClientBuilder SetUsername(string Username);
+        IClientBuilder SetIme(string FirstName);
+        IClientBuilder SetPrezime(string LastName);
+        IClientBuilder SetId(int Id);
+        Client Build();
+    }
+}
