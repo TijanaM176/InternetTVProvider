e1000ab0502bdba9ccd812a1fbdb53d83171ad78
tmarkovic176 <117023727+TijanaM176@users.noreply.github.com>
2024-02-02 16:47:00 +0100

    Odradjene klase Client i Sunscriptions

--- InternetTVSolution/InternetTVProviderLibrary/Models/Client.cs
+++ InternetTVSolution/InternetTVProviderLibrary/Models/Client.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternetTVProviderLibrary.Models
+namespace InternetTVProviderLibrary
 {
-    public class Klijent
+    public class Client
     {
+        [Key]
         public int Id { get; set; }
         public string Username {  get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public Klijent(string username, string ime, string prezime, int id)
+        public Client(string username, string firstname, string lastname, int id)
         {
             Username = username;
-            FirstName = ime;
-            LastName = prezime;
+            FirstName = firstname;
+            LastName = lastname;
             Id = id;
         }
 
