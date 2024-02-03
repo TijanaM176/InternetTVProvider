978c29a3169c86e8b374b8862a978920e9726b18
milena.djukic <djukicmilenaaa@gmail.com>
2024-02-03 11:31:49 +0100

    singleton pattern

---
+++ InternetTVSolution/InternetTVProviderLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿using InternetTVProviderLibrary.SingletonPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTVProviderLibrary
+{
+     class Program
+    {
+
+            static void Main()
+            {
+                DatabaseMenager databaseMenager = DatabaseMenager.Instance;
+
+                //MYSQL
+                string connectionString = "server=localhost;user=root;password=dizajniranje24;database=InternetTVProvider";
+                databaseMenager.ConnectToDatabase("mysql", connectionString);
+
+                //SQLite
+                string sqliteConnectionString = "Data Source=InternetTVProvider.db;Version=3;";
+                databaseMenager.ConnectToDatabase("sqlite", sqliteConnectionString);
+
+
+        }
+
+
+    }
+}
