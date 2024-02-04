a640a908b35d4c75ca7c55294c4f3bc3dfef6241
TijanaM176 <117023727+TijanaM176@users.noreply.github.com>
2024-02-04 21:25:44 +0100

    Ispravljeni namespace-ovi

--- InternetTVSolution/InternetTVProviderLibrary/Models/Package.cs
+++ InternetTVSolution/InternetTVProviderLibrary/Models/Package.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternetTVProviderLibrary
+namespace InternetTVProviderLibrary.Models
 {
     public class Package
     {
