f6b36b4c08f379895e050b56b883e5d809306075
kataa00 <kzdravkovic1@gmail.com>
2024-02-04 22:43:47 +0100

    Dodat proxy pattern

---
+++ InternetTVSolution/InternetTVProviderLibrary/ProxyPattern/IQuery.cs
@@ -0,0 +1,22 @@
+﻿using InternetTVProviderLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTVProviderLibrary.ProxyPattern
+{
+    public interface IQuery
+    {
+        List<Client> getAllClients();
+        List<Package> getAllPackages();
+        void addNewClient(Client newClient);
+        void addNewPackage(Package newPackage);
+        Package getPackageByPackageID(int paketID);
+        int getTypeID(string typeName);
+        void removePackage(int paketID);
+        PackageType getPackageTypeByID(int id);
+        Client getClientByUsername(string username);
+    }
+}
