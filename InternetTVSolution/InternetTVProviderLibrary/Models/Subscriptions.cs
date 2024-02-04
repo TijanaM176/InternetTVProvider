e31c9adb9e8d1ac2443edd0d6b8a0a31bbaeb92f
TijanaM176 <117023727+TijanaM176@users.noreply.github.com>
2024-02-04 21:52:06 +0100

    Dodat activated u Pretplatama

--- InternetTVSolution/InternetTVProviderLibrary/Models/Subscriptions.cs
+++ InternetTVSolution/InternetTVProviderLibrary/Models/Subscriptions.cs
@@ -17,12 +17,16 @@ namespace InternetTVProviderLibrary.Models
         [Required]
         public int packageId { get; set; }
         [ForeignKey("packageId")]
+
+        public bool activated { get; set; }
         Package package { get; set; }
 
-        public Subscriptions(int clientId, int packageId)
+
+        public Subscriptions(int clientId, int packageId,bool activated)
         {
             this.clientId = clientId;
             this.packageId = packageId;
+            this.activated = activated;
         }
     }
 }
