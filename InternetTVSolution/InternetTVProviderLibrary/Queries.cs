35cc05eed8192d0b61a09144181e926a79cf6039
Hristina <126361505+hristina71@users.noreply.github.com>
2024-02-05 12:22:44 +0100

    dodavanje upita za citanje svih klijenata i dodavanje novog

--- InternetTVSolution/InternetTVProviderLibrary/Queries.cs
+++ InternetTVSolution/InternetTVProviderLibrary/Queries.cs
@@ -1,4 +1,6 @@
-﻿using InternetTVProviderLibrary.SingletonPattern;
+﻿using InternetTVProviderLibrary.BuilderPattern;
+using InternetTVProviderLibrary.Models;
+using InternetTVProviderLibrary.SingletonPattern;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
@@ -17,5 +19,71 @@ namespace InternetTVProviderLibrary
         {
             this.connection = connection;
         }
+
+        public List<Client> getAllClients()
+        {
+            List<Client> clients = new List<Client>();
+
+            connection.Open();
+
+            String query = @"select * from Clients";
+
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+
+            DbDataReader reader = dbCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                clients.Add(new ClientBuilder()
+                                 .SetId(reader.GetInt32(0))
+                                 .SetUsername(reader.GetString(1))
+                                 .SetIme(reader.GetString(2))
+                                 .SetPrezime(reader.GetString(3))
+                                 .Build()
+                    );
+            }
+
+            connection.Close();
+            return clients;
+        }
+
+        public void insertNewClient(Client cl)
+        {
+            connection.Open();
+
+            String query = "insert into Clients (Username, FirstName, LastName) values (@Username, @FirstName, @LastName)";
+
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+
+
+            DbParameter username, firstName, lastName;
+
+            username = dbCommand.CreateParameter();
+            username.ParameterName = "@Username";
+            username.Value = cl.Username;
+
+            firstName = dbCommand.CreateParameter();
+            firstName.ParameterName = "@FirstName";
+            firstName.Value = cl.FirstName;
+
+            lastName = dbCommand.CreateParameter();
+            lastName.ParameterName = "@LastName";
+            lastName.Value = cl.LastName;
+
+            dbCommand.Parameters.Add(username);
+            dbCommand.Parameters.Add(firstName);
+            dbCommand.Parameters.Add(lastName);
+
+            int succes = dbCommand.ExecuteNonQuery();
+
+            if (succes > 0) { Console.WriteLine("Korisnik dodat u bazu"); }
+            else { Console.WriteLine("Greska prilikom dodavanja korisnika u bazu!"); }
+
+            connection.Close();
+        }
     }
 }
+
