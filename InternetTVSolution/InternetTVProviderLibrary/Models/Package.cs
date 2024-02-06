87408694ff1762587d8852ca5b26d203ef78b482
TijanaM176 <117023727+TijanaM176@users.noreply.github.com>
2024-02-06 11:03:44 +0100

    Izmenjene klase u Models

--- InternetTVSolution/InternetTVProviderLibrary/Models/Package.cs
+++ InternetTVSolution/InternetTVProviderLibrary/Models/Package.cs
@@ -11,24 +11,18 @@ namespace InternetTVProviderLibrary.Models
     public class Package
     {
         [Key]
-        public int ID { get; }
-
-        [ForeignKey("PackageTypeID")]
-        public PackageType PackageType { get; }
-
+        public int ID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public int NumberOfChannels { get; set; }
-        public int InternetSpeed { get; set; }
+        [ForeignKey("PackageTypeID")]
         public int PackageTypeID { get; set; }
 
-        public Package(int ID, string Name, double Price, int NumberOfChannels, int InternetSpeed, int PackageTypeID)
+        
+        public Package(int ID, string Name, double Price, int PackageTypeID)
         {
             this.ID = ID;
             this.Name = Name;
             this.Price = Price;
-            this.NumberOfChannels = NumberOfChannels;
-            this.InternetSpeed = InternetSpeed;
             this.PackageTypeID = PackageTypeID;
         }
 
---
+++ InternetTVSolution/InternetTVProviderLibrary/Models/TVPackage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTVProviderLibrary.Models
+{
+    public class TVPackage : Package
+    {
+        public int NumberOfChannels { get; set; }
+
+        public TVPackage(int id, string name, double price, int numberOfChannels, int packageTypeId)
+            : base(id, name, price,packageTypeId)
+        {
+            NumberOfChannels = numberOfChannels;
+        }
+    }
+
+}
