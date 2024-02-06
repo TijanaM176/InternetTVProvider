ecd3f37359fb49f4dea5fec6ae315b8a0d2fa1f7
kataa00 <kzdravkovic1@gmail.com>
2024-02-06 11:41:32 +0100

    Dodate klase za InternetPackage i CombinedPackage

---
+++ InternetTVSolution/InternetTVProviderLibrary/Models/CombinedPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTVProviderLibrary.Models
+{
+    public class CombinedPackage : Package
+    {
+        [ForeignKey("TVPackage")]
+        public int TVPackageId { get; set; }
+
+        [ForeignKey("InternetPackage")]
+        public int InternetPackageId { get; set; }
+
+        public CombinedPackage(int id, string name, double price, int tvPackageId, int internetPackageId, int packageTypeId)
+            : base(id, name, price, packageTypeId)
+        {
+            TVPackageId = tvPackageId;
+            InternetPackageId = internetPackageId;
+        }
+    }
+
+}
