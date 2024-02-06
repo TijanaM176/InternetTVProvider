924aef6c04c42e210ff5642dbda4b01578722931
Hristina <126361505+hristina71@users.noreply.github.com>
2024-02-06 21:20:22 +0100

    dodavanje metoda u QueryFacade klasu

--- InternetTVSolution/InternetTVProviderLibrary/FacadePattern/QueryFacade.cs
+++ InternetTVSolution/InternetTVProviderLibrary/FacadePattern/QueryFacade.cs
@@ -1,11 +1,14 @@
-﻿using InternetTVProviderLibrary.Models;
+﻿using InternetTVProviderLibrary.BuilderPattern;
+using InternetTVProviderLibrary.Models;
 using InternetTVProviderLibrary.ProxyPattern;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace InternetTVProviderLibrary.FacadePattern
 {
@@ -55,5 +58,65 @@ namespace InternetTVProviderLibrary.FacadePattern
             return queries.getAllClients();
         }
 
+        public void addNewClient(string username, string firstname, string lastname)
+        {
+            Client cl = new ClientBuilder().SetUsername(username).SetIme(firstname).SetPrezime(lastname).Build();
+
+            queries.addNewClient(cl);
+        }
+
+        public void addNewTVPackage(string name, double price, int numberOfChannels, int packageTypeId)
+        {
+            TVPackageBuilder packageBuilder = new TVPackageBuilder();
+
+            packageBuilder.SetName(name);
+            packageBuilder.SetPrice(price);
+            packageBuilder.SetNumberOfChanels(numberOfChannels);
+            packageBuilder.SetTypeID(packageTypeId);
+
+           TVPackage tvPackage = packageBuilder.Build();
+
+            queries.addNewTVPackage(tvPackage);
+        }
+
+        public void addNewInternetPackage(string name, double price, int internetSpeed, int packageTypeId)
+        {
+            InternetPackageBuilder packageBuilder = new InternetPackageBuilder();
+
+            packageBuilder.SetName(name);
+            packageBuilder.SetPrice(price);
+            packageBuilder.SetInternetSpeed(internetSpeed);
+            packageBuilder.SetTypeID(packageTypeId);
+
+            InternetPackage internetPackage = packageBuilder.Build();
+
+            queries.addNewInternetPackage(internetPackage);
+        }
+
+        public void addNewCombinedPackage(string name, double price, int tvPackageId, int internetPackageId, int packageTypeId)
+        {
+            CombinedPackageBuilder packageBuilder = new CombinedPackageBuilder();
+
+            packageBuilder.SetName(name);
+            packageBuilder.SetPrice(price);
+            packageBuilder.SetTvPackageID(tvPackageId);
+            packageBuilder.SetInternetPackageID(internetPackageId);
+            packageBuilder.SetTypeID(packageTypeId);
+
+            CombinedPackage combinedPackage = packageBuilder.Build();
+        }
+
+        public double getAllSubscriptionsPriceByClient(int clientId)
+        {
+            List<Package> packages = new List<Package>();
+            double sum = 0.0;
+
+            packages = queries.getSubscribedPackagesByClientId(clientId);
+
+            foreach(Package package in packages) { sum += package.Price; }
+            
+            return sum; 
+
+        }
     }
 }
