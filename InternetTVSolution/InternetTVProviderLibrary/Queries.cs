e44fc51acb9e187ce226c997d21c673d53404c67
Hristina <126361505+hristina71@users.noreply.github.com>
2024-02-06 15:36:58 +0100

    dodati upiti za razlicite tipove paketa

--- InternetTVSolution/InternetTVProviderLibrary/Queries.cs
+++ InternetTVSolution/InternetTVProviderLibrary/Queries.cs
@@ -120,14 +120,14 @@ namespace InternetTVProviderLibrary
             return foundClient;
         }
 
-        public List<Package> getAllPackages()
+        public List<TVPackage> getAllTVPackages()
         {
 
-            List<Package> packages = new List<Package>();
+            List<TVPackage> packages = new List<TVPackage>();
 
             connection.Open();
 
-            String query = @"select * from Packages";
+            String query = @"select * from TVPackage";
 
             DbCommand dbCommand = connection.CreateCommand();
             dbCommand.CommandText = query;
@@ -136,32 +136,134 @@ namespace InternetTVProviderLibrary
 
             while (reader.Read())
             {
+                TVPackageBuilder package = new TVPackageBuilder();
 
-                packages.Add(new PackageBuilder()
-                                 .SetID(reader.GetInt32(0))
-                                 .SetName(reader.GetString(1))
-                                 .SetPrice(reader.GetDouble(2))
-                                 .SetInternetSpeed(reader.GetInt32(3))
-                                 .SetNumberOfChannels(reader.GetInt32(4))
-                                 .SetTypeID(reader.GetInt32(3))  
-                                 .Build()
-                    );
+                package.SetID(reader.GetInt32(0));
+                package.SetName(reader.GetString(1));
+                package.SetPrice(reader.GetDouble(2));
+                package.SetNumberOfChanels(reader.GetInt32(3));
+                package.SetTypeID(reader.GetInt32(4));
+
+                packages.Add(package.Build());
             }
 
             connection.Close();
             return packages;
         }
 
-        public void insertNewPackage(Package pack)
+        public List<InternetPackage> getAllInternetPackages()
         {
+
+            List<InternetPackage> packages = new List<InternetPackage>();
+
             connection.Open();
 
-            String query = @"insert into Packages (Name, Price, InternetSpeed, NumberOfChannels, TypeID) values (@Name, @Price, @InternetSpeed, @NumberOfChannels, @PackageTypeID)";
+            String query = @"select * from InternetPackage";
 
             DbCommand dbCommand = connection.CreateCommand();
             dbCommand.CommandText = query;
 
-            DbParameter name, price, internetSpeed, numberOfChannels, typeID;
+            DbDataReader reader = dbCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                InternetPackageBuilder package = new InternetPackageBuilder();
+
+                package.SetID(reader.GetInt32(0));
+                package.SetName(reader.GetString(1));
+                package.SetPrice(reader.GetDouble(2));
+                package.SetInternetSpeed(reader.GetInt32(3));
+                package.SetTypeID(reader.GetInt32(4));
+
+                packages.Add(package.Build());
+            }
+
+            connection.Close();
+            return packages;
+        }
+
+        public List<CombinedPackage> getAllCombinedPackages()
+        {
+
+            List<CombinedPackage> packages = new List<CombinedPackage>();
+
+            connection.Open();
+
+            String query = @"select * from CombinedPackage";
+
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+
+            DbDataReader reader = dbCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                CombinedPackageBuilder package = new CombinedPackageBuilder();
+
+                package.SetID(reader.GetInt32(0));
+                package.SetName(reader.GetString(1));
+                package.SetPrice(reader.GetDouble(2));
+                package.SetTvPackageID(reader.GetInt32(3));
+                package.SetInternetPackageID(reader.GetInt32(4));
+                package.SetTypeID(reader.GetInt32(5));
+
+                packages.Add(package.Build());
+            }
+
+            connection.Close();
+            return packages;
+        }
+
+        public void addNewTVPackage(TVPackage pack)
+        {
+            connection.Open();
+
+            String query = @"insert into TVPackage (Name, Price, NumberOfChannels, TypeID) values (@Name, @Price, @NumberOfChannels, @PackageTypeID)";
+
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+
+            DbParameter name, price, numberOfChannels, typeID;
+
+            name = dbCommand.CreateParameter();
+            name.ParameterName = "@Name";
+            name.Value = pack.Name;
+
+            price = dbCommand.CreateParameter();
+            price.ParameterName = "@Price";
+            price.Value = pack.Price;
+
+            numberOfChannels = dbCommand.CreateParameter();
+            numberOfChannels.ParameterName = "@NumberOfChannels";
+            numberOfChannels.Value = pack.NumberOfChannels;
+
+            typeID = dbCommand.CreateParameter();
+            typeID.ParameterName = "@PackageTypeID";
+            typeID.Value = pack.PackageTypeID;
+
+            dbCommand.Parameters.Add(name);
+            dbCommand.Parameters.Add(price);
+            dbCommand.Parameters.Add(numberOfChannels);
+            dbCommand.Parameters.Add(typeID);
+
+            int succes = dbCommand.ExecuteNonQuery();
+
+            if (succes > 0) { Console.WriteLine("TVPaket dodat u bazu"); }
+            else { Console.WriteLine("Greska prilikom dodavanja TV paketa u bazu!"); }
+
+            connection.Close();
+        }
+
+        public void addNewInternetPackage(InternetPackage pack)
+        {
+            connection.Open();
+
+            String query = @"insert into InternetPackage (Name, Price, InternetSpeed, TypeID) values (@Name, @Price, @InternetSpeed, @PackageTypeID)";
+
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+
+            DbParameter name, price, internetSpeed, typeID;
 
             name = dbCommand.CreateParameter();
             name.ParameterName = "@Name";
@@ -175,10 +277,6 @@ namespace InternetTVProviderLibrary
             internetSpeed.ParameterName = "@InternetSpeed";
             internetSpeed.Value = pack.InternetSpeed;
 
-            numberOfChannels = dbCommand.CreateParameter();
-            numberOfChannels.ParameterName = "@NumberOfChannels";
-            numberOfChannels.Value = pack.NumberOfChannels;
-
             typeID = dbCommand.CreateParameter();
             typeID.ParameterName = "@PackageTypeID";
             typeID.Value = pack.PackageTypeID;
@@ -186,17 +284,61 @@ namespace InternetTVProviderLibrary
             dbCommand.Parameters.Add(name);
             dbCommand.Parameters.Add(price);
             dbCommand.Parameters.Add(internetSpeed);
-            dbCommand.Parameters.Add(numberOfChannels);
             dbCommand.Parameters.Add(typeID);
 
             int succes = dbCommand.ExecuteNonQuery();
 
-            if (succes > 0) { Console.WriteLine("Paket dodat u bazu"); }
-            else { Console.WriteLine("Greska prilikom dodavanja paketa u bazu!"); }
+            if (succes > 0) { Console.WriteLine("Internet Paket dodat u bazu"); }
+            else { Console.WriteLine("Greska prilikom dodavanja Internet paketa u bazu!"); }
 
             connection.Close();
         }
 
+        public void addNewCombinedPackage(CombinedPackage pack)
+        {
+            connection.Open();
+
+            String query = @"insert into InternetPackage (Name, Price, TVPackageId, InternetPackageId, TypeID) values (@Name, @Price, @TVPackageId, @InternetPackageId, @PackageTypeID)";
+
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+
+            DbParameter name, price, tvPackageId, internetPackageId, typeID;
+
+            name = dbCommand.CreateParameter();
+            name.ParameterName = "@Name";
+            name.Value = pack.Name;
+
+            price = dbCommand.CreateParameter();
+            price.ParameterName = "@Price";
+            price.Value = pack.Price;
+
+            tvPackageId = dbCommand.CreateParameter();
+            tvPackageId.ParameterName = "@TVPackageId";
+            tvPackageId.Value = pack.TVPackageId;
+
+            internetPackageId = dbCommand.CreateParameter();
+            internetPackageId.ParameterName = "@InternetPackageId";
+            internetPackageId.Value = pack.InternetPackageId;
+
+            typeID = dbCommand.CreateParameter();
+            typeID.ParameterName = "@PackageTypeID";
+            typeID.Value = pack.PackageTypeID;
+
+            dbCommand.Parameters.Add(name);
+            dbCommand.Parameters.Add(price);
+            dbCommand.Parameters.Add(tvPackageId);
+            dbCommand.Parameters.Add(internetPackageId);
+            dbCommand.Parameters.Add(typeID);
+
+            int succes = dbCommand.ExecuteNonQuery();
+
+            if (succes > 0) { Console.WriteLine("Kombinovani Paket dodat u bazu"); }
+            else { Console.WriteLine("Greska prilikom dodavanja Kombinovanog paketa u bazu!"); }
+
+            connection.Close();
+        }
+        /*
         Package getPackageByPackageID(int packID)
         {
             Package foundPackage = null;
@@ -258,6 +400,6 @@ namespace InternetTVProviderLibrary
 
             connection.Close();
         }
-    }
+    }*/
 }
 
